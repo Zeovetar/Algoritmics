4bcc8b84eb7ceb2730f6295b68cad51aca95a8a1
Zeovetar <zeovetar@gmail.com>
2021-11-01 09:56:21 +0300

    recursion done

--- lesson1/fibonachi/Program.cs
+++ lesson1/fibonachi/Program.cs
@@ -1,21 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 
-/*private static int[] memF = new int[100];
-private static void Main(string[] args)
-{
-    Console.WriteLine(Fib(10));
-}
-
-private static int Fib(int n)
-{
-    if (n <= 1) return n;
-
-    if (memF[n] != 0) return memF[n];
-
-    memF[n] = Fib(n - 2) + Fib(n - 1);
-    return memF[n];
-}*/
 
 namespace fibonachi
 {
@@ -24,7 +9,13 @@ namespace fibonachi
         static void Main(string[] args)
         {
             List<int> fiblist = new List<int>();
-            Console.WriteLine(result(Convert.ToInt32(Console.ReadLine()), fiblist));
+            Console.WriteLine("Рассчет числа фибоначчи! Введите N!");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                fiblist.Add(0);
+            }
+            Console.WriteLine(result(n, fiblist));
         }
 
 
@@ -33,12 +24,7 @@ namespace fibonachi
             if (n == 1 || n == 0) return n;
 
             if (fiblist.Count >= n && fiblist[n] != 0) return fiblist[n];
-            for (int i = 0; i < n; i++)
-            {
-                fiblist.Add(0);
-            }
-
-            fiblist.Add(result(n - 2, fiblist) + result(n - 1, fiblist));
+            fiblist[n] = result(n - 2, fiblist) + result(n - 1, fiblist);
             return fiblist[n];
         }
     }
