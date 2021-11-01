c432fa69116c6fa24a5cc33e160624ea8a943a5b
Zeovetar <zeovetar@gmail.com>
2021-11-01 19:39:10 +0300

    done

--- lesson1/fibonachi/Program.cs
+++ lesson1/fibonachi/Program.cs
@@ -4,8 +4,79 @@ using System.Collections.Generic;
 
 namespace fibonachi
 {
+    public class TestCase
+    {
+        public int X { get; set; }
+        public int Expected { get; set; }
+        public Type ExpectedException { get; set; }
+    }
+
+
     class Program
     {
+        static void TestFibRecursive(TestCase testCase)
+        {
+            List<int> testlist = new List<int>();
+            try
+            {
+                var actual = resultRecursive(testCase.X, testlist);
+
+                if (actual == testCase.Expected)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (StackOverflowException)
+            {
+                   Console.WriteLine("VALID TEST");
+
+            }
+            catch (Exception ex)
+            {
+                if (testCase.ExpectedException != null)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            
+        }
+
+        static void TestFibLoop(TestCase testCase)
+        {
+            List<int> testlist = new List<int>();
+            try
+            {
+                var actual = resultLoop(testCase.X);
+
+                if (actual == testCase.Expected)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (testCase.ExpectedException != null && ex.GetType() == testCase.ExpectedException)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+        }
         static void Main(string[] args)
         {
             List<int> fiblist = new List<int>();
@@ -15,19 +86,76 @@ namespace fibonachi
             {
                 fiblist.Add(0);
             }
-            Console.WriteLine(result(n, fiblist));
+            Console.WriteLine(resultRecursive(n, fiblist));
+            Console.WriteLine(resultLoop(n));
+
+            var testCase1 = new TestCase() //не понимаю, почему он invalid. туплю.
+            {
+                X = 8,
+                Expected = 21,
+                ExpectedException = null
+            };
+            var testCase2 = new TestCase()
+            {
+                X = 0,
+                Expected = 0,
+                ExpectedException = null
+            };
+            var testCase3 = new TestCase()
+            {
+                X = 1,
+                Expected = 1,
+                ExpectedException = null
+            };
+            var testCase4 = new TestCase()
+            {
+                X = 8,
+                Expected = 45,
+                ExpectedException = null
+            };
+            var testCase5 = new TestCase()
+            {
+                X = 1000000,
+                Expected = 21,
+                ExpectedException = typeof(System.StackOverflowException) //оно не ловится, ступил
+            };
+            Console.WriteLine("\n\n tests==============================================================================================");
+            TestFibRecursive(testCase1);
+            TestFibRecursive(testCase2);
+            TestFibRecursive(testCase3);
+            TestFibRecursive(testCase4);
+            //TestFibRecursive(testCase5);
+
+            Console.WriteLine("===================================================================================================");
+
+            TestFibLoop(testCase1);
+            TestFibLoop(testCase2);
+            TestFibLoop(testCase3);
+            TestFibLoop(testCase4);
+            TestFibLoop(testCase5);
+            Console.WriteLine("===================================================================================================");
         }
 
 
-        static int result(int n, List<int> fiblist)
+        static int resultRecursive(int n, List<int> fiblist)
         {
             if (n == 1 || n == 0) return n;
 
             if (fiblist.Count >= n && fiblist[n] != 0) return fiblist[n];
-            fiblist[n] = result(n - 2, fiblist) + result(n - 1, fiblist);
+            fiblist[n] = resultRecursive(n - 2, fiblist) + resultRecursive(n - 1, fiblist);
             return fiblist[n];
         }
+
+        static int resultLoop(int n)
+        {
+            int a = 0, b = 1, c = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c;
+        }
     }
-
-
 }
